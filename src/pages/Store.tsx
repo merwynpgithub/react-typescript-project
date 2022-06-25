9a4837d7732a0745b184b03f8cd06229f9a5be10
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-25 11:45:28 -0400

    update store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,16 @@
+import { Col, Row } from 'react-bootstrap'
+import StoreItem from '../components/StoreItem'
+import items from '../data/items.json'
+
 function Store() {
-  return <h2>Store</h2>
+  return (
+    <>
+      <h2>Store</h2>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {items.map(item => <Col key={item.id}><StoreItem {...item} /></Col>)}
+      </Row>
+    </>
+  )
 }
 
 export default Store
