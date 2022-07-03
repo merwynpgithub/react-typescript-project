2ceba8c10764169377cebfc6e0f58046a174c9c2
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-07-03 12:31:30 -0400

    add conditional rendering for cart quantity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,16 @@ function Navbar() {
       <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
       <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
     </Nav>
-    <Button style={{ width: "3rem", height: "3rem", position: "relative" }} variant="outline-primary" className="rounded-circle">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" fill="#000"></path> </svg>
+    <Button
+      onClick={openCart} 
+      style={{ width: "3rem", height: "3rem", position: "relative" }} variant="outline-primary" className="rounded-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" fill="#000"></path> </svg>
+    {cartQuantity > 0 &&
     <div className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
     style={{color: "white", height: "1.5rem", width: "1.5rem", position: "absolute", bottom: "0", right: "0", transform: "translate(25%, 25%)"}}
     >
       {cartQuantity}
-    </div>
+    </div>}
     </Button>
     </Container>
   </NavbarBs>
